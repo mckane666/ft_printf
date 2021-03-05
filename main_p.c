24d69cfdf2efe06fd9d9b7e32c9bd9328941d6a1
Mckane666 <Pedro>
2021-03-05 21:29:22 +0000

    prtf5

--- main_p.c
+++ main_p.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int a;
 	int b;
@@ -15,24 +15,33 @@ int	main(void)
 	int h;
 	char *flag = "-0.*";
 	printf("\n");
-	// a = ft_printf("%p %15p %-15p", -2, -2, -2);
-	// b = printf("%p %15p %-15p", -2, -2, -2);
-	// printf("\n\n");
-	// printf("\nft_printf = %d\nprintf = %d\n", a, b);
-
-	// c = ft_printf("%p %15p %-15p", 0, 0, 0);
-	// d = printf("%p %15p %-15p", 0, 0, 0);
-	// printf("\n\n");
-	// printf("\nft_printf = %d\nprintf = %d\n", c, d);
-
-	// e = ft_printf("%p %15p %-15p", 2, 2, 2);
-	// f = printf("%p %15p %-15p", 2, 2, 2);
-	// printf("\n\n");
-	// printf("\nft_printf = %d\nprintf = %d", e, f);
-
-	g = ft_printf("%*p %*p",15,-15, &flag, &flag);
-	h = ft_printf("%*p %*p",15,-15, &flag, &flag);
-	printf("\n\n");
-	printf("\nft_printf = %d\nprintf = %d", g, h);
+	if (ft_strncmp(argv[1], "1", 1) == 0)
+	{
+		a = ft_printf("%p %15p %-15p", -2, -2, -2);
+		b = printf("%p %15p %-15p", -2, -2, -2);
+		printf("\n");
+		printf("\nft_printf = %d\nprintf = %d\n", a, b);
+	}
+	else if (ft_strncmp(argv[1], "2", 1) == 0)
+	{
+		c = ft_printf("%p %15p %-15p", 0, 0, 0);
+		d = printf("%p %15p %-15p", 0, 0, 0);
+		printf("\n");
+		printf("\nft_printf = %d\nprintf = %d\n", c, d);
+	}
+	else if (ft_strncmp(argv[1], "3", 1) == 0)
+	{
+		e = ft_printf("%p %15p %-15p", 2, 2, 2);
+		f = printf("%p %15p %-15p", 2, 2, 2);
+		printf("\n");
+		printf("\nft_printf = %d\nprintf = %d", e, f);
+	}
+	else if (ft_strncmp(argv[1], "4", 1) == 0)
+	{
+		g = ft_printf("%*p %*p",20, &flag,-20, &flag);
+		h = printf("%*p %*p",20, &flag,-20, &flag);
+		printf("\n");
+		printf("\nft_printf = %d\nprintf = %d", g, h);
+	}
 }
 	
