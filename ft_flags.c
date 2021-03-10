9ec527a5551f78cd30f6998c79493226a4590329
Mckane666 <Pedro>
2021-03-10 14:20:17 +0000

    prtf6

--- ft_flags.c
+++ ft_flags.c
@@ -20,8 +20,8 @@ void	ft_conversions(t_printf *pf, va_list ap)
 		ft_is_str(pf, ap);
 	else if (ft_strchr("p", pf->get_args[pf->index])) //
 		ft_is_p(pf, ap);
-	// // else if (ft_strchr("d", pf->get_args[pf->index])) //
-	// //   	ft_is_d(pf, ap);
+	else if (ft_strchr("d", pf->get_args[pf->index])) //
+	  	ft_is_d(pf, ap);
 	// else if (ft_strchr("i", pf->get_args[pf->index])) //
 	// 	ft_is_i(pf, ap);
 	// else if (ft_strchr("u", pf->get_args[pf->index])) //
@@ -171,7 +171,7 @@ void	ft_is_x(t_printf *pf, va_list ap)
 		pf->x = va_arg(ap, unsigned long);
 	ft_hexa(pf->x, pf);
 	pf->str_len = ft_strlen(pf->str);
-	if ((pf->precision >= 0 && pf->precision < pf->str_len - pf->point))
+	if (pf->precision >= 0 && pf->precision < pf->str_len - pf->point)
 		pf->precision = pf->str_len;
 	pf->retu = ft_calloc(sizeof(char), pf->str_len + pf->width + pf->precision + pf->point + 1 + pf->ast);
 	while (pf->width-- > pf->precision && !pf->minus)
@@ -187,28 +187,31 @@ void	ft_is_x(t_printf *pf, va_list ap)
 	free(pf->retu);
 }
 
-// // void	ft_is_d(t_printf *pf, va_list ap)
-// // {	
-// // 	if (pf->get_args[pf->index] == 'd')
-// // 		pf->i = va_arg(ap, int);
-// // 	pf->str = ft_itoa(pf->i);
-// // 	pf->str_len = ft_strlen(pf->str);
-// // 	if ((pf->precision >= 0 && pf->precision < pf->str_len - 1))
-// // 		pf->precision = pf->str_len;
-// // 	pf->retu = ft_calloc(sizeof(char), pf->str_len + pf->width + pf->point + 1 + pf->ast);
-// // 	while (pf->width-- > pf->precision && !pf->minus)
-// // 		pf->retu[pf->k++] = ' ';
-// // 	while (pf->precision > pf->str_len++)
-// // 		pf->retu[pf->k++] = ZERO_NO[(pf->zero && !pf->minus) || pf->str];
-// // 	while (pf->str[pf->j] && pf->j < pf->precision )
-// // 		pf->retu[pf->k++] = pf->str[pf->j++];
-// // 	while (pf->width-- >= pf->precision && pf->minus)
-// // 		pf->retu[pf->k++] = ' ';
-// // 	pf->retu[pf->k] = 0;
-// // 	if (pf->minus)
-// // 		pf->retu = ft_strjoin("-", pf->retu);
-// // 	pf->cont += ft_strlen(pf->retu);
-// // 	ft_putstr(pf->retu);
-// // 	ft_init_printf_flags(pf);
-// // 	free(pf->retu);
-// // }
+void	ft_is_d(t_printf *pf, va_list ap)
+{	
+	if (pf->get_args[pf->index] == 'd')
+		pf->i = va_arg(ap, int);
+	pf->str = ft_itoa(pf->i);
+	pf->str_len = ft_strlen(pf->str);
+	if (pf->str[0] == '-')
+		pf->j++;
+	if ((pf->precision >= 0 && pf->precision < pf->str_len - pf->point))
+		pf->precision = pf->str_len;
+	pf->retu = ft_calloc(sizeof(char), pf->str_len + pf->width + pf->precision);
+	while (pf->width-- > pf->precision && !pf->minus)
+		pf->retu[pf->k++] = ' ';
+	while (pf->precision > pf->str_len++)
+		pf->retu[pf->k++] = ZERO_NO[(pf->zero && !pf->minus) || pf->str];
+	while (pf->str[pf->j] && pf->j < pf->precision)
+		pf->retu[pf->k++] = pf->str[pf->j++];
+	while (pf->width-- >= pf->precision && pf->minus)
+		pf->retu[pf->k++] = ' ';
+	pf->retu[pf->k] = 0;
+	if (pf->str[0] == '-')
+		pf->retu = ft_strjoin("-", pf->retu);
+	free(pf->str);
+	pf->cont += ft_strlen(pf->retu);
+	ft_putstr(pf->retu);
+	ft_init_printf_flags(pf);
+	free(pf->retu);
+}
